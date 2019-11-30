5ab70ab29baaecf256eb1e438bb2302cb56426f0
Franata <rosandi.mahardika@gmail.com>
2019-12-01 00:18:30 +0700

    Update

--- app/Barang.php
+++ app/Barang.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class barang extends Model
+class Barang extends Model
 {
     protected $table = 'barang';
     protected $primaryKey = 'id_barang';
